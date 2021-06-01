f3270cdd1dde46d0b05395a78401cdc87cc9f30a
MobDe <MobDevX.outlook.com>
2021-06-01 11:02:32 +0700

    Добавлен слушатель нажатия кнопки

--- app/src/main/java/kt/firstk/MainActivity.kt
+++ app/src/main/java/kt/firstk/MainActivity.kt
@@ -2,21 +2,22 @@ package kt.firstk
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var tv : TextView
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tv = findViewById(R.id.textView)
-
-    }
-    fun buttonOnClick(view: View) {
-        tv.text = "Нажата кнопка Ok"
+        // 2) Добавить кнопку, findViewByID и слушателя:
+        button = findViewById(R.id.button)
+        button.setOnClickListener(View.OnClickListener { tv.setText("Нажата кнопка Ok") })
     }
 }

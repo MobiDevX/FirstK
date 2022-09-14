7a8221fb94f2250645c70e6dbafde5dafceeff71
MobDe <MobDevX.outlook.com>
2022-09-14 10:47:34 +0700

    Upgrade to API 33

--- app/src/main/java/kt/firstk/MainActivity.kt
+++ app/src/main/java/kt/firstk/MainActivity.kt
@@ -8,16 +8,14 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textView : TextView
-    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.tv)
+        val textView : TextView = findViewById(R.id.tv)
         // 2) Добавить кнопку, findViewByID и слушателя:
-        button = findViewById(R.id.button)
+        val button : Button = findViewById(R.id.button)
         button.setOnClickListener(View.OnClickListener { textView.text="Нажата кнопка Ok" })
     }
 }

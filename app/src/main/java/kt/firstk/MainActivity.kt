4fc68079a7770895f0807c99df8d8ef2007efc8c
MobDe <MobDevX.outlook.com>
2022-09-20 12:49:19 +0700

    Button handler

--- app/src/main/java/kt/firstk/MainActivity.kt
+++ app/src/main/java/kt/firstk/MainActivity.kt
@@ -7,34 +7,27 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView : TextView = findViewById(R.id.tv)
+        textView = findViewById(R.id.tv)
+        val button = findViewById<Button>(R.id.button)
+        // 3) Создать слушателя:
+        button.setOnClickListener(bListener)
 
-        val x = arrayOf(3, 5, 2)
-        //val x = Array(3) {it} // результат: 0, 1, 2,
-        x[2] = 7
-        x[0] = x[1]+x[2] // результат: 12, 5, 7,
-
-
-        val b = arrayOfNulls<Int>(3)
-        b[2] = 7
-        b[0] = (b[1]?: 5) + b[2]!! // результат: 12, null, 7,
-
-        // val list = listOf('D', 'H', 'A')
-
-
-        // 2) Добавить кнопку, findViewByID и слушателя:
-        val button : Button = findViewById(R.id.button)
-        button.setOnClickListener(View.OnClickListener {
-            var str = " "
-            for(i in 0..2) { str += (x[i].toString() + ", ")}
-            str += ("\n" + x.size)
-            textView.text= str
-        })
-
+        // 2) Добавить слушателя для кнопки:
+/*        button.setOnClickListener(View.OnClickListener {
+            textView.text= "ClickListener: Нажата кнопка"
+        })*/
     }
+    private val bListener  = View.OnClickListener() {
+        textView.text= "bListener: Нажата кнопка"
+    }
+    // 1) Добавить функцию для обработки нажатия кнопки:
+/*    fun buttonClick(view: View) {
+        textView.text= "Нажата кнопка"
+    }*/
 }
